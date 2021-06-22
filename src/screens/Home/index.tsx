b7167b1f252220837847503f574b93e0bae8c990
pedrofbaltar <pfbaltariead@gmail.com>
2021-06-22 19:27:14 -0300

    Category select

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,19 +1,32 @@
-import React from "react";
-import {
-  View
-} from "react-native";
+import React, { useEffect } from "react";
+import { View } from "react-native";
 import { Profile } from '../../components/Profile';
 import { ButtonAdd } from '../../components/ButtonAdd';
+import { CategorySelect } from '../../components/CategorySelect';
 
 import { styles } from "./styles";
+import { useState } from "react";
 
 
 export function Home() {
+  const [category, setCategory] = useState('');
+
+  function handleCategorySelect(categoryId: string) {
+    categoryId === category ? setCategory('') : setCategory(categoryId);
+  }
+
   return (
     <View>
       <View style={styles.header}>
         <Profile />
         <ButtonAdd />
+      </View>
+
+      <View>
+        <CategorySelect
+          categorySelected={category}
+          setCategory={handleCategorySelect}
+        />
       </View>
     </View>
   );
