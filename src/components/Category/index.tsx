43b8902a04f32d918973617db02f42e10c225cc2
pedrofbaltar <pfbaltariead@gmail.com>
2021-06-23 08:45:36 -0300

    Fixing Category Cards

--- src/components/Category/index.tsx
+++ src/components/Category/index.tsx
@@ -10,17 +10,19 @@ import { categories } from '../../utils/categories';
 
 type Props = RectButtonProps & {
   title: string;
-  icon: React.FC<SvgProps>
-  checked?: boolean
+  icon: React.FC<SvgProps>;
+  hasCheckBox?: boolean;
+  checked?: boolean;
 }
 
 export function Category({
   title,
   icon: Icon,
+  hasCheckBox = false,
   checked = true,
   ...rest
 }: Props) {
-  const { secondary50, secondary70 } = theme.colors;
+  const { secondary40, secondary50, secondary70, secondary85 } = theme.colors;
 
   return (
     <RectButton {...rest}>
@@ -28,8 +30,14 @@ export function Category({
       style={styles.container}
       colors={[secondary50, secondary70]}
       >
-        <View style={[styles.content, { opacity: checked ? 1 : 0.4 }]}>
-          <View style={checked ? styles.checked : styles.check} />
+        <LinearGradient
+          style={[styles.content, { opacity: checked ? 1 : 0.5 }]}
+          colors={[ checked ? secondary85 : secondary50, secondary40 ]}
+        > 
+          {
+            hasCheckBox && 
+            <View style={checked ? styles.checked : styles.check} />
+          }
 
           <Icon
             width={48}
@@ -37,7 +45,7 @@ export function Category({
           />
 
           <Text style={styles.title}>{title}</Text>
-        </View>
+        </LinearGradient>
       </LinearGradient>
     </RectButton>
   );
